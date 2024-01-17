55b0429a4b13d0c8ba89ee5c7a3a7ccc96b2f0c5
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-01-17 11:46:06 +0200

    Added mockito special cases demo

--- src/main/java/testingil/unittesting/examples/demos/d02/mocking/Driver.java
+++ src/main/java/testingil/unittesting/examples/demos/d02/mocking/Driver.java
@@ -14,7 +14,7 @@ public class Driver {
 		return !car.isRunning();
 	}
 
-	public boolean hasKeys() {
+	public static boolean hasKeys() {
 		return true;
 	}
 	
--- src/test/java/testingil/unittesting/examples/demos/d02/mocking/mockito/Test_with_mockito_special.java
+++ src/test/java/testingil/unittesting/examples/demos/d02/mocking/mockito/Test_with_mockito_special.java
@@ -1,6 +1,7 @@
 package testingil.unittesting.examples.demos.d02.mocking.mockito;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import testingil.unittesting.examples.demos.d02.mocking.ACMode;
@@ -9,35 +10,38 @@ import testingil.unittesting.examples.demos.d02.mocking.Car;
 import testingil.unittesting.examples.demos.d02.mocking.Driver;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
-class Test_with_mockito_verification{
+class Test_with_mockito_special {
 
 	@Test
-	void AC_is_set_before_we_drive() {
-		Car mockCar = mock(Car.class);
-		Driver driver = new Driver(mockCar);
-		driver.drive();
-		
-		Mockito.verify(mockCar,atLeast(1)).
-			setAC(any(AirCondition.class));
+	void cannot_drive_without_keys() {
+		Driver driver = new Driver(null);
+		try( var mockHandler = mockStatic(Driver.class)){
+			when(Driver.hasKeys()).thenReturn(false);
+			assertThat(driver.canDrive()).isFalse();
+		}
 	}
 
-	
 	@Test
-	void AC_is_set_on_before_we_drive() {
+	void AC_is_set_off_before_we_drive() {
 		Car mockCar = mock(Car.class);
 		Driver driver = new Driver(mockCar);
-		ArgumentCaptor<AirCondition> acCaptor = 
+
+		ArgumentCaptor<AirCondition> acCaptor =
 				ArgumentCaptor.forClass(AirCondition.class);
-		
-		driver.drive();
-		Mockito.verify(mockCar).setAC(acCaptor.capture());
-		
-		AirCondition ac = acCaptor.getValue();
-		assertThat(ac.getMode()).isEqualTo(ACMode.On);
-	}
 
+		try(var mockHandler = mockConstruction(AirCondition.class,
+				(mock,context)-> {
+			when(mock.getMode()).thenReturn(ACMode.Off);
+			}
+		)){
+			driver.drive();
+		}
+
+		verify(mockCar).setAC(acCaptor.capture());
+
+		AirCondition ac = acCaptor.getValue();
+		assertThat(ac.getMode()).isEqualTo(ACMode.Off);
+	}
 }
